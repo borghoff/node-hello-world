2bbe971d22fe99077d4ead42781895a892b62265
bueroborghoff <thomas@borghoff.de>
2025-02-13 15:55:35 +0100

    Adding dotenv/config

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import http from 'http';
+import 'dotenv/config';
 
 const port = process.env.PORT || 3000;
 
+console.log(`Trying to run app on port ${port}`);
+
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hier gibt es immer noch nichts zu sehen, bitte gehen Sie weiter ... Version 12 \n");
+  response.end("Hier gibt es immer noch nichts zu sehen, bitte gehen Sie weiter ... Version 13 \n");
 }).listen(port);
 
 console.log(`App is running... (port: ${port})`);
