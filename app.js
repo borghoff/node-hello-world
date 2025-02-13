cc8fe02a28b212a9e7a9d00ff7b0599ab6f480fc
bueroborghoff <thomas@borghoff.de>
2025-02-13 15:40:31 +0100

    commonjs -> module

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const http = require('http');
+import http from 'http';
 
 const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hier gibt es immer noch nichts zu sehen, bitte gehen Sie weiter ...\n");
+  response.end("Hier gibt es immer noch nichts zu sehen, bitte gehen Sie weiter ... Version 12 \n");
 }).listen(port);
 
 console.log(`App is running... (port: ${port})`);
